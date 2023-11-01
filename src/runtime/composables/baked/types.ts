635bc0713c4c87694ef0c0ff9933e0ef6ae5ebdb
Steven John <stevenjpx2@gmail.com>
2023-11-01 23:02:47 +0530

    refactor: add defineTween for creating types

---
+++ src/runtime/composables/baked/types.ts
@@ -0,0 +1,147 @@
+import type { Direction } from "../../types";
+
+const defineTween = <
+  Obj extends Record<string, { from: gsap.TweenVars; to: gsap.TweenVars }>,
+  T extends Exclude<keyof Obj, "DEFAULT">,
+>(
+  x: Obj,
+  defaultTween: T,
+) => ({ ...x, DEFAULT: defaultTween });
+
+export type AnimationOptions = {
+  /** Animation opacity
+   * @remarks
+   * - `in` means opacity from 0 to 1
+   * - `out` means opacity from 1 to 0
+   * - `true` is the same as `in`
+   * */
+  opacity?: "in" | "out" | true;
+  /** Animation translate
+   * @remarks
+   * - `bottom` means translate from bottom to top
+   * - `top` means translate from top to bottom
+   * - `left` means translate from left to right
+   * - `right` means translate from right to left
+   * - `true` is the same as `bottom`
+   * */
+  translate?: Direction | true;
+  /** Animation skew
+   * @remarks
+   * - `bottom` means skew from bottom to top
+   * - `top` means skew from top to bottom
+   * - `left` means skew from left to right
+   * - `right` means skew from right to left
+   * - `true` is the same as `bottom`
+   * */
+  skew?: Direction | true;
+  /** Animation scale
+   * @remarks
+   * - `out` means scale from 0.5 to 1
+   * - `in` means scale from 1.5 to 1
+   * - `true` is the same as `out`
+   * */
+  scale?: "out" | "in" | true;
+  /** Animation rotate
+   * @remarks
+   * - `left` means rotate from -15deg to 0deg
+   * - `right` means rotate from 15deg to 0deg
+   * - `true` is the same as `left`
+   * */
+  rotate?: "left" | "right" | true;
+  /** Animation blur
+   * @remarks
+   * - `in` means blur from 10px to 0px
+   * - `out` means blur from 0px to 10px
+   * - `true` is the same as `in`
+   * */
+  blur?: "in" | "out" | true;
+};
+
+type RequiredAnimationOptions = Required<AnimationOptions>;
+
+export const presetTweens = {
+  opacity: defineTween(
+    {
+      in: { from: { autoAlpha: 0 }, to: { autoAlpha: 1 } },
+      out: { from: { autoAlpha: 1 }, to: { autoAlpha: 0 } },
+    },
+    "in",
+  ),
+  translate: defineTween(
+    {
+      bottom: {
+        from: { y: "200%" },
+        to: {
+          y: 0,
+          scrollTrigger: {
+            start: "top-=250% center",
+            end: "bottom center",
+            markers: false,
+          },
+        },
+      },
+      top: {
+        from: {
+          y: "-200%",
+          scrollTrigger: {
+            start: "top+=50% center",
+            end: "bottom center",
+            markers: false,
+          },
+        },
+        to: { y: 0 },
+      },
+      left: { from: { x: "-200%" }, to: { x: 0 } },
+      right: { from: { x: "200%" }, to: { x: 0 } },
+    },
+    "bottom",
+  ),
+  skew: defineTween(
+    {
+      bottom: { from: { skewY: 10 }, to: { skewY: 0 } },
+      top: { from: { skewY: -10 }, to: { skewY: 0 } },
+      left: { from: { skewX: -10 }, to: { skewX: 0 } },
+      right: { from: { skewX: 10 }, to: { skewX: 0 } },
+    },
+    "bottom",
+  ),
+  scale: defineTween(
+    {
+      in: { from: { scale: 0 }, to: { scale: 1 } },
+      out: { from: { scale: 1 }, to: { scale: 0 } },
+    },
+    "in",
+  ),
+  rotate: defineTween(
+    {
+      left: {
+        from: { rotation: 15, transformOrigin: "top left" },
+        to: { rotation: 0 },
+      },
+      right: {
+        from: { rotation: -15, transformOrigin: "top right" },
+        to: { rotation: 0 },
+      },
+    },
+    "left",
+  ),
+  blur: defineTween(
+    {
+      in: { from: { filter: "blur(10px)" }, to: { filter: "blur(0px)" } },
+      out: { from: { filter: "blur(0px)" }, to: { filter: "blur(10px)" } },
+    },
+    "in",
+  ),
+} satisfies {
+  [K in keyof RequiredAnimationOptions]: {
+    [P in RequiredAnimationOptions[K] as Exclude<
+      RequiredAnimationOptions[K],
+      true
+    >]: {
+      from: gsap.TweenVars;
+      to: gsap.TweenVars;
+    };
+  } & {
+    DEFAULT: Exclude<RequiredAnimationOptions[K], true>;
+  };
+};
--- src/runtime/composables/useBakedAnimation.ts
+++ src/runtime/composables/useBakedAnimation.ts
@@ -1,55 +1,6 @@
-import type { Direction } from "../types";
 import { type MaybeRefOrGetter, useGsap } from "#imports";
 import type { StrongTweenVars } from "./useGsap";
-
-export type AnimationOptions = {
-  /** Animation opacity
-   * @remarks
-   * - `in` means opacity from 0 to 1
-   * - `out` means opacity from 1 to 0
-   * - `true` is the same as `in`
-   * */
-  opacity?: "in" | "out" | true;
-  /** Animation translate
-   * @remarks
-   * - `bottom` means translate from bottom to top
-   * - `top` means translate from top to bottom
-   * - `left` means translate from left to right
-   * - `right` means translate from right to left
-   * - `true` is the same as `bottom`
-   * */
-  translate?: Direction | true;
-  /** Animation skew
-   * @remarks
-   * - `bottom` means skew from bottom to top
-   * - `top` means skew from top to bottom
-   * - `left` means skew from left to right
-   * - `right` means skew from right to left
-   * - `true` is the same as `bottom`
-   * */
-  skew?: Direction | true;
-  /** Animation scale
-   * @remarks
-   * - `out` means scale from 0.5 to 1
-   * - `in` means scale from 1.5 to 1
-   * - `true` is the same as `out`
-   * */
-  scale?: "out" | "in" | true;
-  /** Animation rotate
-   * @remarks
-   * - `left` means rotate from -15deg to 0deg
-   * - `right` means rotate from 15deg to 0deg
-   * - `true` is the same as `left`
-   * */
-  rotate?: "left" | "right" | true;
-  /** Animation blur
-   * @remarks
-   * - `in` means blur from 10px to 0px
-   * - `out` means blur from 0px to 10px
-   * - `true` is the same as `in`
-   * */
-  blur?: "in" | "out" | true;
-};
+import { presetTweens, type AnimationOptions } from "./baked/types";
 
 /** Generates the baked animation tweens
  * @param animationOptions - The animation options
@@ -60,97 +11,28 @@ function generateAnimationTweens(animationOptions: AnimationOptions): {
   from: StrongTweenVars;
   to: StrongTweenVars;
 } {
-  const { opacity, translate, skew, scale, rotate, blur } = animationOptions;
   const tweens: {
     from: StrongTweenVars;
     to: StrongTweenVars;
   } = { from: {}, to: {} };
 
-  if (opacity) {
-    if (opacity === "in" || opacity === true) {
-      tweens.from.autoAlpha = 0;
-      tweens.to.autoAlpha = 1;
-    } else if (opacity === "out") {
-      tweens.from.autoAlpha = 1;
-      tweens.to.autoAlpha = 0;
-    }
-  }
+  Object.entries(animationOptions)
+    .filter(([_, val]) => !!val)
+    .map(([key, value]) => {
+      const tween = presetTweens[key as keyof typeof presetTweens];
+      let tweenKey: string;
+      if (typeof value === "boolean") {
+        tweenKey = tween.DEFAULT;
+      } else {
+        tweenKey = value;
+      }
 
-  if (translate) {
-    if (translate === "bottom" || translate === true) {
-      tweens.from.y = "200%";
-      tweens.to.y = 0;
-      tweens.to.scrollTrigger = {
-        start: "top-=250% center",
-        end: "bottom center",
-        markers: false,
-      };
-    } else if (translate === "top") {
-      tweens.from.y = "-200%";
-      tweens.to.scrollTrigger = {
-        start: "top+=50% center",
-        end: "bottom center",
-        markers: false,
-      };
-      tweens.to.y = 0;
-    }
-    if (translate === "left") {
-      tweens.from.x = "-200%";
-      tweens.to.x = 0;
-    } else if (translate === "right") {
-      tweens.from.x = "200%";
-      tweens.to.x = 0;
-    }
-  }
+      // @ts-expect-error
+      tweens.from = { ...tweens.from, ...tween[tweenKey].from };
 
-  if (skew) {
-    if (skew === "bottom" || skew === true) {
-      tweens.from.skewY = 10;
-      tweens.to.skewY = 0;
-    } else if (skew === "top") {
-      tweens.from.skewY = -10;
-      tweens.to.skewY = 0;
-    }
-    if (skew === "left") {
-      tweens.from.skewX = -10;
-      tweens.to.skewX = 0;
-    } else if (skew === "right") {
-      tweens.from.skewX = 10;
-      tweens.to.skewX = 0;
-    }
-  }
-
-  if (scale) {
-    if (scale === "out" || scale === true) {
-      tweens.from.scale = 0.5;
-      tweens.to.scale = 1;
-    } else if (scale === "in") {
-      tweens.from.scale = 1.5;
-      tweens.to.scale = 1;
-    }
-  }
-
-  if (rotate) {
-    if (rotate === "left" || rotate === true) {
-      tweens.from.transformOrigin = "top left";
-      tweens.from.rotation = 15;
-      tweens.to.rotation = 0;
-    } else if (rotate === "right") {
-      tweens.from.transformOrigin = "bottom right";
-      tweens.from.rotation = -15;
-      tweens.to.rotation = 0;
-    }
-  }
-
-  if (blur) {
-    if (blur === "in" || blur === true) {
-      tweens.from.filter = "blur(10px)";
-      tweens.to.filter = "blur(0px)";
-    } else if (blur === "out") {
-      tweens.from.filter = "blur(0px)";
-      tweens.to.filter = "blur(10px)";
-    }
-  }
+      // @ts-expect-error
+      tweens.to = { ...tweens.to, ...tween[tweenKey].to };
+    });
 
   return tweens;
 }
